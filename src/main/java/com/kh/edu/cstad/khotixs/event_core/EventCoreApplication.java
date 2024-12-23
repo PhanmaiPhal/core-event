d805bb3addcb281b6c989c7f49684e51949188d5
PhanmaiPhal <phannmai576@gmail.com>
2024-12-23 16:44:39 +0700

    comit

---
+++ src/main/java/com/kh/edu/cstad/khotixs/event_core/EventCoreApplication.java
@@ -0,0 +1,13 @@
+package com.kh.edu.cstad.khotixs.event_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kh/edu/cstad/khotixs/event_core/EventCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.edu.cstad.khotixs.event_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
